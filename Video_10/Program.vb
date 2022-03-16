4e1bb6110bc376679d79c448565a671198d7ecd0
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-15 21:52:39 -0500

    Video 10

--- Video_10/Program.vb
+++ Video_10/Program.vb
@@ -2,6 +2,13 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Video 10 - More Math Operators
+        Dim myDouble As Double = 11
+        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        myDouble += 23
+        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        myDouble *= 126
+        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        Console.ReadLine()
     End Sub
 End Module
