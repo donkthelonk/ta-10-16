c5f264a747d17ac6ac240344e81031ade6fc4261
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-15 22:11:20 -0500

    Video 12

--- Video_10/Program.vb
+++ Video_10/Program.vb
@@ -12,19 +12,30 @@ Module Program
         'Console.ReadLine()
 
         'Video 11 - Getting User Input
-        Dim userName As String = Nothing
-        Dim age As Integer = Nothing
-        Dim userSalary As Double = Nothing
-        Console.WriteLine("What is your name?")
-        userName = Console.ReadLine()
-        Console.WriteLine("What is your age?")
-        age = Console.ReadLine()
-        Console.WriteLine("What is your salary?")
-        userSalary = Console.ReadLine()
-        Console.Write("Your name is: " & userName)
-        Console.Write("    Your age is: " & age)
-        Console.Write("    Your salary is: " & userSalary)
-        Console.WriteLine()
+        'Dim userName As String = Nothing
+        'Dim age As Integer = Nothing
+        'Dim userSalary As Double = Nothing
+        'Console.WriteLine("What is your name?")
+        'userName = Console.ReadLine()
+        'Console.WriteLine("What is your age?")
+        'age = Console.ReadLine()
+        'Console.WriteLine("What is your salary?")
+        'userSalary = Console.ReadLine()
+        'Console.Write("Your name is: " & userName)
+        'Console.Write("    Your age is: " & age)
+        'Console.Write("    Your salary is: " & userSalary)
+        'Console.WriteLine()
+        'Console.ReadLine()
+
+        'Video 12 - Creating a Basic Calculator
+        Console.WriteLine("Please enter the first number: ")
+        Dim num1 As Double = Console.ReadLine()
+        Console.WriteLine("First number: " & num1)
+        Console.WriteLine("Please enter the second number: ")
+        Dim num2 As Double = Console.ReadLine()
+        Console.WriteLine("Second number: " & num2)
+        Dim answer As Double = num1 * num2
+        Console.WriteLine("The numbers multiplied is: " & num1 * num2)
         Console.ReadLine()
     End Sub
 End Module
