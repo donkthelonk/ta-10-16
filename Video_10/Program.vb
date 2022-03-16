6fb3fd90c9d562608319f3ed582f09c0660fe9d3
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-15 22:05:06 -0500

    Video 11

--- Video_10/Program.vb
+++ Video_10/Program.vb
@@ -3,12 +3,28 @@ Imports System
 Module Program
     Sub Main(args As String())
         'Video 10 - More Math Operators
-        Dim myDouble As Double = 11
-        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
-        myDouble += 23
-        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
-        myDouble *= 126
-        Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        'Dim myDouble As Double = 11
+        'Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        'myDouble += 23
+        'Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        'myDouble *= 126
+        'Console.WriteLine("Initial Value: 11 New Value " & myDouble)
+        'Console.ReadLine()
+
+        'Video 11 - Getting User Input
+        Dim userName As String = Nothing
+        Dim age As Integer = Nothing
+        Dim userSalary As Double = Nothing
+        Console.WriteLine("What is your name?")
+        userName = Console.ReadLine()
+        Console.WriteLine("What is your age?")
+        age = Console.ReadLine()
+        Console.WriteLine("What is your salary?")
+        userSalary = Console.ReadLine()
+        Console.Write("Your name is: " & userName)
+        Console.Write("    Your age is: " & age)
+        Console.Write("    Your salary is: " & userSalary)
+        Console.WriteLine()
         Console.ReadLine()
     End Sub
 End Module
